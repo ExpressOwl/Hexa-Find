347ce518fc860109c42f54965cd2e5d6bebeebf2
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-05-09 20:11:53 +0100

    Getting random colour hexadecimals

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,24 @@ import "./App.css";
 function App() {
   const [color, setColor] = useState("");
 
+  // Math.random() returns a random number between 0-16777215
+  // Math.floor() rounds it down to an integer
+  // toString(16) converts the number to hexadecimal
+  // Add a # before the color and you get a random color everytime.
   const getRandomColor = () => {
-    const digits = [
-      "0",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-    ];
+    const color = Math.floor(Math.random() * 16777215).toString(16);
+    return "#" + color;
   };
 
   useEffect(() => {
     // Generate random color
-    setColor("#FF00DD");
+    setColor(getRandomColor());
   }, []);
 
   return (
-    <div className="App">
-      <div className="guess-me" style={{ background: color }}></div>
+    <div className="flex h-screen justify-center items-center">
+      <div className="w-60 h-60 m-auto" style={{ background: color }}></div>
+      <span></span>
     </div>
   );
 }
