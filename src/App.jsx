378bb91af64100ee6716caacdc55f5b280fe53da
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-05-09 21:37:32 +0100

    Styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   const [color, setColor] = useState("");
   const [answers, setAnswers] = useState([]);
-  const [result, setResult] = useState(false);
+  const [result, setResult] = useState(undefined);
 
   // Math.random() returns a random number between 0-16777215
   // Math.floor() rounds it down to an integer
@@ -35,34 +35,36 @@ function App() {
 
   function handleAnswer(answer) {
     if (answer === color) {
-      setResult(false);
+      setResult(true);
       // If correct, next question
       generateColor();
     } else {
-      setResult(true);
+      setResult(false);
     }
   }
 
   return (
     <>
       <div className="flex h-screen justify-center items-center bg-background bg-cover bg-no-repeat bg-blend-multiply bg-gray-400">
-        <div>
+        <div className="bg-[#111827] p-8 rounded-md shadow-md">
           <div
-            className="w-60 h-60 m-auto mb-8"
+            className="w-72 h-72 m-auto mb-8 rounded-sm"
             style={{ background: color }}
           ></div>
           {answers.map((answer) => (
             <button
               onClick={() => handleAnswer(answer)}
               key={answer}
-              className="mx-2 p-2 bg-orange-500 mb-4"
+              className="text-yellow-400 uppercase hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900 justify-evenly"
             >
               {answer}
             </button>
           ))}
-          {result === false && <div className="text-red-500">Wrong Color</div>}
+          {result === false && (
+            <div className="text-red-500 text-center">Not quite, the answer was {color}</div>
+          )}
           {result === true && (
-            <div className="text-blue-400">Color Wizard!</div>
+            <div className="text-blue-400 text-center">Color Wizard!</div>
           )}
         </div>
       </div>
